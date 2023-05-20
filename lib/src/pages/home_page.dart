3ecb6d1982bc6eccf519a6b78fa77e0b9b32b8cf
Nazrul Islam <nnakib78@gmail.com>
2023-05-20 22:02:12 +0600

    dooooone

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -13,12 +13,6 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Text('Background Service Example'),
       ),
-      body: Center(
-        child: Text('Background Service Example'),
-      ),
-      body: Center(
-        child: Text('Background Service Example'),
-      ),
     );
   }
 }
