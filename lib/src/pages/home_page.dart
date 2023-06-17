9f41846beb0d24e36cc29192ca373d87f9ca2bfb
Nazrul Islam <nnakib78@gmail.com>
2023-06-17 23:28:54 +0600

    djygfcwugc

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -15,5 +15,25 @@ class HomePage extends StatelessWidget {
       ),
   
     );
+      body: Center(
+        child: Text('Background Service Example'),
+      ),
+  
+    );
+      body: Center(
+        child: Text('Background Service Example'),
+      ),
+  
+    );
+      body: Center(
+        child: Text('Background Service Example'),
+      ),
+  
+    );
+      body: Center(
+        child: Text('Background Service Example'),
+      ),
+  
+    );
   }
 }
