0e4db187e2347d48c5e7ce76d71b6791261a5845
Nazrul Islam <nnakib78@gmail.com>
2023-08-25 23:38:00 +0600

    changed,,

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -13,36 +13,6 @@ class HomePage extends StatelessWidget {
       body: const Center(
         child: Text('Background Service Example'),
       ),
-      // body: const Center(
-      //   child: Text('Background Service Example'),
-      // ),
-      // body: const Center(
-      //   child: Text('Background Service Example'),
-      // ),
-      // body: const Center(
-      //   child: Text('Background Service Example'),
-      // ),
-      // body: const Center(
-      //   child: Text('Background Service Example'),
-      // ),
-      // body: const Center(
-      //   child: Text('Background Service Example'),
-      // ),
-      // body: const Center(
-      //   child: Text('Background Service Example'),
-      // ),
-      // body: const Center(
-      //   child: Text('Background Service Example'),
-      // ),
-      // body: const Center(
-      //   child: Text('Background Service Example'),
-      // ),
-      // body: const Center(
-      //   child: Text('Background Service Example'),
-      // ),
-      // body: const Center(
-      //   child: Text('Background Service Example'),
-      // ),
     );
   }
 }
