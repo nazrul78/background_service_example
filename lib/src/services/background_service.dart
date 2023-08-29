a4f67dacdfccc7eadb946a9d0078bc930b676109
Nazrul Islam <nnakib78@gmail.com>
2023-08-30 00:25:29 +0600

    Added bg plugin

--- lib/src/services/background_service.dart
+++ lib/src/services/background_service.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter_background_service/flutter_background_service.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class BackgroundService {
@@ -75,75 +77,75 @@ class BackgroundService {
     // SharedPreferences preferences = await SharedPreferences.getInstance();
     // await preferences.setString("hello", "world");
 
-    // /// OPTIONAL when use custom notification
-    // final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    //     FlutterLocalNotificationsPlugin();
+    /// OPTIONAL when use custom notification
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
 
-    // if (service is AndroidServiceInstance) {
-    //   service.on('setAsForeground').listen((event) {
-    //     service.setAsForegroundService();
-    //   });
+    if (service is AndroidServiceInstance) {
+      service.on('setAsForeground').listen((event) {
+        service.setAsForegroundService();
+      });
 
-    //   service.on('setAsBackground').listen((event) {
-    //     service.setAsBackgroundService();
-    //   });
-    // }
+      service.on('setAsBackground').listen((event) {
+        service.setAsBackgroundService();
+      });
+    }
 
-    // service.on('stopService').listen((event) {
-    //   service.stopSelf();
-    // });
+    service.on('stopService').listen((event) {
+      service.stopSelf();
+    });
 
-    // // bring to foreground
-    // Timer.periodic(const Duration(seconds: 1), (timer) async {
-    //   if (service is AndroidServiceInstance) {
-    //     if (await service.isForegroundService()) {
-    //       /// OPTIONAL for use custom notification
-    //       /// the notification id must be equals with AndroidConfiguration when you call configure() method.
-    //       flutterLocalNotificationsPlugin.show(
-    //         888,
-    //         'COOL SERVICE',
-    //         'Awesome ${DateTime.now()}',
-    //         const NotificationDetails(
-    //           android: AndroidNotificationDetails(
-    //             'my_foreground',
-    //             'MY FOREGROUND SERVICE',
-    //             icon: 'ic_bg_service_small',
-    //             ongoing: true,
-    //           ),
-    //         ),
-    //       );
+    // bring to foreground
+    Timer.periodic(const Duration(seconds: 1), (timer) async {
+      if (service is AndroidServiceInstance) {
+        if (await service.isForegroundService()) {
+          /// OPTIONAL for use custom notification
+          /// the notification id must be equals with AndroidConfiguration when you call configure() method.
+          flutterLocalNotificationsPlugin.show(
+            888,
+            'COOL SERVICE',
+            'Awesome ${DateTime.now()}',
+            const NotificationDetails(
+              android: AndroidNotificationDetails(
+                'my_foreground',
+                'MY FOREGROUND SERVICE',
+                icon: 'ic_bg_service_small',
+                ongoing: true,
+              ),
+            ),
+          );
 
-    //       // if you don't using custom notification, uncomment this
-    //       service.setForegroundNotificationInfo(
-    //         title: "My App Service",
-    //         content: "Updated at ${DateTime.now()}",
-    //       );
-    //     }
-    //   }
+          // if you don't using custom notification, uncomment this
+          service.setForegroundNotificationInfo(
+            title: "My App Service",
+            content: "Updated at ${DateTime.now()}",
+          );
+        }
+      }
 
-    //   /// you can see this log in logcat
-    //   print('FLUTTER BACKGROUND SERVICE: ${DateTime.now()}');
+      /// you can see this log in logcat
+      print('FLUTTER BACKGROUND SERVICE: ${DateTime.now()}');
 
-    //   // test using external plugin
-    //   final deviceInfo = DeviceInfoPlugin();
-    //   String? device;
-    //   if (Platform.isAndroid) {
-    //     final androidInfo = await deviceInfo.androidInfo;
-    //     device = androidInfo.model;
-    //   }
+      // test using external plugin
+      // final deviceInfo = DeviceInfoPlugin();
+      String? device;
+      // if (Platform.isAndroid) {
+      //   final androidInfo = await deviceInfo.androidInfo;
+      //   device = androidInfo.model;
+      // }
 
-    //   if (Platform.isIOS) {
-    //     final iosInfo = await deviceInfo.iosInfo;
-    //     device = iosInfo.model;
-    //   }
+      // if (Platform.isIOS) {
+      //   final iosInfo = await deviceInfo.iosInfo;
+      //   device = iosInfo.model;
+      // }
 
-    //   service.invoke(
-    //     'update',
-    //     {
-    //       "current_date": DateTime.now().toIso8601String(),
-    //       "device": device,
-    //     },
-    //   );
-    // });
+      service.invoke(
+        'update',
+        {
+          "current_date": DateTime.now().toIso8601String(),
+          "device": device,
+        },
+      );
+    });
   }
 }
