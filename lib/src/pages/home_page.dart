dfa191731398ae8a7f8163e20b3fedea3cfe98a2
Nazrul Islam <nnakib78@gmail.com>
2023-08-31 00:48:44 +0600

    add ping

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,8 +1,17 @@
+import 'dart:developer';
+
+import 'package:dart_ping/dart_ping.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String res = 'UnKnown';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,9 +19,34 @@ class HomePage extends StatelessWidget {
         title: const Text('Background Service Example'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Background Service Example'),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 15),
+            Text('Ping results:'),
+            Text(res),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            log('Presssssssssss');
+
+            final result = await Ping('google.com', count: 1).stream.first;
+
+            log('${result.response}');
+
+            setState(() {
+              res = result.response.toString();
+            });
+            // final ping = Ping('google.com', count: 5);
+
+            // // Begin ping process and listen for output
+            // ping.stream.listen((event) {
+            //   log('${event}');
+            // });
+          },
+          child: Icon(Icons.add)),
     );
   }
 }
