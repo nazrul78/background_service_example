56bc730a4a3eafb9c6ec189993109343607dbcd9
Nazrul Islam <nnakib78@gmail.com>
2023-08-31 22:46:20 +0600

    changed

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,3 @@
-import 'dart:developer';
-
-import 'package:dart_ping/dart_ping.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +8,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String res = 'UnKnown';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,34 +15,7 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Background Service Example'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 15),
-            Text('Ping results:'),
-            Text(res),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            log('Presssssssssss');
-
-            final result = await Ping('google.com', count: 1).stream.first;
-
-            log('${result.response}');
-
-            setState(() {
-              res = result.response.toString();
-            });
-            // final ping = Ping('google.com', count: 5);
-
-            // // Begin ping process and listen for output
-            // ping.stream.listen((event) {
-            //   log('${event}');
-            // });
-          },
-          child: Icon(Icons.add)),
+      body: const Center(child: Text('Home')),
     );
   }
 }
