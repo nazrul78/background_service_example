99164b828bedc9a4896ecdd8f0912bebd42852d2
nazrul78 <nnakib78@gmail.com>
2025-07-09 15:40:16 +0600

    Home page modified.

--- lib/service/pages/home_page.dart
+++ lib/service/pages/home_page.dart
@@ -5,6 +5,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Home page')),
+      body: Center(child: Text('Home Page')),
+    );
   }
 }
