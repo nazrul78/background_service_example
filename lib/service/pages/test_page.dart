c44bba198d70555313a830e1cc8c6c255bbc1475
nazrul78 <nnakib78@gmail.com>
2025-07-22 16:08:17 +0600

    Changed ...

--- lib/service/pages/test_page.dart
+++ lib/service/pages/test_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Login page')),
-      body: Column(children: [Center(child: Text('Login Page'))]),
+      // body: Column(children: [Center(child: Text('Login Page'))]),
     );
   }
 }
