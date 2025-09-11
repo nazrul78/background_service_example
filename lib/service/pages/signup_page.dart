211e6075589d5f40e92dcbaa3edddc9743c1a999
nazrul78 <nnakib78@gmail.com>
2025-09-11 14:39:52 +0600

    Removed unn data.

--- lib/service/pages/signup_page.dart
+++ lib/service/pages/signup_page.dart
@@ -282,28 +282,6 @@ class HomePage extends StatelessWidget {
               labelText: 'Employee id',
             ),
           ),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Employee id',
-            ),
-          ),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Employee id',
-            ),
-          ),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Employee id',
-            ),
-          ),
-        
         ],
       ),
     );
