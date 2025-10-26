8e074888ab90347dd01176bc7a7288614101ea40
nazrul78 <nnakib78@gmail.com>
2025-10-26 16:58:00 +0600

    Changed...

--- lib/service/pages/signup_page.dart
+++ lib/service/pages/signup_page.dart
@@ -254,20 +254,6 @@ class HomePage extends StatelessWidget {
               labelText: 'Employee id',
             ),
           ),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Employee id',
-            ),
-          ),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Employee id',
-            ),
-          ),
         ],
       ),
     );
