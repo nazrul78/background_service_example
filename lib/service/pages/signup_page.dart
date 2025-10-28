6dea0ab2bae04dbe47d93d90895de361314d1b9e
nazrul78 <nnakib78@gmail.com>
2025-10-28 16:53:43 +0600

    Updated the signup page.

--- lib/service/pages/signup_page.dart
+++ lib/service/pages/signup_page.dart
@@ -233,20 +233,6 @@ class HomePage extends StatelessWidget {
               labelText: 'Employee id',
             ),
           ),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Employee id',
-            ),
-          ),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Employee id',
-            ),
-          ),
         ],
       ),
     );
