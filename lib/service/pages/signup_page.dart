fb5aa42f579be196a7e3333f34bacb306c5cec9f
nazrul78 <nnakib78@gmail.com>
2025-12-09 16:54:15 +0600

    Changed...

--- lib/service/pages/signup_page.dart
+++ lib/service/pages/signup_page.dart
@@ -86,20 +86,7 @@ class HomePage extends StatelessWidget {
               labelText: 'Email',
             ),
           ),
-          TextField(
-            //obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Email',
-            ),
-          ),
-          TextField(
-            //obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Email',
-            ),
-          ),
+      
         ],
       ),
     );
