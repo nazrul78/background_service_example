8a4a7e1657d5b8d9e5877ee331a1849646dddc8a
Nazrul Islam <nnakib78@gmail.com>
2026-01-19 22:00:34 +0600

    shviuhv

--- lib/service/pages/signup_page.dart
+++ lib/service/pages/signup_page.dart
@@ -51,20 +51,6 @@ class HomePage extends StatelessWidget {
               labelText: 'User Name',
             ),
           ),
-          TextField(
-            //obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'User Name',
-            ),
-          ),
-          TextField(
-            //obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'User Name',
-            ),
-          ),
         ],
       ),
     );
