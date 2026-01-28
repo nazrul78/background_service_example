63797f666049df6fdfab7647b53885fc74ef8836
Nazrul Islam <nnakib78@gmail.com>
2026-01-28 21:13:06 +0600

    updated

--- lib/service/pages/test_page.dart
+++ lib/service/pages/test_page.dart
@@ -31,21 +31,6 @@ class TestPage extends StatelessWidget {
                 labelText: 'User Name',
               ),
             ),
-
-            TextField(
-              //obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'User Name',
-              ),
-            ),
-            TextField(
-              //obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'User Name',
-              ),
-            ),
           ],
         ),
       ),
