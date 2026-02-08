6cfc23b12a4b3f03e4bf21210ce27ab5c2f78639
nazrul78 <nnakib78@gmail.com>
2026-02-08 16:50:03 +0600

    Updated....

--- lib/service/pages/signup_page.dart
+++ lib/service/pages/signup_page.dart
@@ -121,20 +121,6 @@ class HomePage extends StatelessWidget {
               labelText: 'User Name',
             ),
           ),
-          TextField(
-            //obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'User Name',
-            ),
-          ),
-          TextField(
-            //obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'User Name',
-            ),
-          ),
         ],
       ),
     );
