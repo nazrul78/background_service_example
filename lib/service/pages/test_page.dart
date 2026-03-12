1128cdc8b9cde9c298a227452902b5abfc78e14f
nazrul78 <nnakib78@gmail.com>
2026-03-12 15:10:28 +0600

    Modified....

--- lib/service/pages/test_page.dart
+++ lib/service/pages/test_page.dart
@@ -39,20 +39,6 @@ class TestPage extends StatelessWidget {
                 labelText: 'User Name',
               ),
             ),
-            TextField(
-              //obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'User Name',
-              ),
-            ),
-            TextField(
-              //obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'User Name',
-              ),
-            ),
           ],
         ),
       ),
