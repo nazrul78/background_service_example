e0b9be590fcbe3638f17ea5e95c03d17ab5d0e2c
nazrul78 <nnakib78@gmail.com>
2026-03-25 16:13:00 +0600

    Updated the test page.

--- lib/service/pages/test_page.dart
+++ lib/service/pages/test_page.dart
@@ -25,6 +25,141 @@ class TestPage extends StatelessWidget {
                 labelText: 'User Name',
               ),
             ),
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
+
+            TextField(
+              //obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'User Name',
+              ),
+            ),
           ],
         ),
       ),
